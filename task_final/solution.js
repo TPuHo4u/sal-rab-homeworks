aee4db6ecd3d1f889339f6e8999ee83b80dbc84c
TPuHo4u <62938203+TPuHo4u@users.noreply.github.com>
2021-09-13 16:27:03 +0300

    FinalTaskKrasyuk_D.S.
    Final

--- task_final/solution.js
+++ task_final/solution.js
@@ -33,18 +33,21 @@
 //    }
 // }
 
-function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
-    let countOfGoods = goods.length;
-    for (let i = 0; i <= countOfGoods; i += 1) {
-    data.goods.push(goods[i].title);
-    data.goods.push(goods[i].count);
-    }
-    data.client = name + phone;
-    data.order.address = address;
-    data.order.sum = sum;
 
-let jsonData = JSON.stringify(data);
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {
+                client: name + ` ${phone}`,
+                order: {
+                    address: `ул. ${address.street}` + `, дом ${address.house}`+`, ${address.entrance} подъезд`+ `, ${address.floor} этаж` + `, кв ${address.flat}`,
+                    sum
+                },
+                goods: []
+    };
+    let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i += 1) {
+        data.goods.push({title:goods[i].title, count:goods[i].count});
+}
+    let jsonData = JSON.stringify({data});
 
 return jsonData;
 }
